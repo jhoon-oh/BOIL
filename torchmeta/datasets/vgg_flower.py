8838a1d0692d07896ac86a5a4a0af87e9d4b918a
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 02:52:23 +0000

    Adding tarfile member sanitization to extractall()

--- torchmeta/datasets/vgg_flower.py
+++ torchmeta/datasets/vgg_flower.py
@@ -127,7 +127,26 @@ class VggFlowerClassDataset(ClassDataset):
 
         filename = os.path.join(self.root, '102flowers.tgz')
         with tarfile.open(filename, 'r') as f:
-            f.extractall(self.root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(f, self.root)
 
         imagelabels_path = self.root+'/imagelabels.mat'
         with open(imagelabels_path, 'rb') as f:
